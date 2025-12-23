13dcc144837abfa1d3d0ea06dfab9bfcc3dd1afa
Bagas Riatma Putera <145095794+Bagasbelajarhtml@users.noreply.github.com>
2025-12-23 23:16:31 +0700

    feat: add Model Guru and Kelas

---
+++ app/Models/Guru.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Guru extends Model
+{
+    protected $fillable = [
+        'user_id',
+        'nip',
+        'nama_guru',
+        'kelas_id',
+        'no_hp',
+    ];
+
+    public function kelas()
+    {
+        return $this->belongsTo(Kelas::class, 'kelas_id');
+    }
+}
---
+++ app/Models/Kelas.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Kelas extends Model
+{
+    use HasFactory;
+    protected $fillable = [
+        'nama_kelas',
+        'wali_kelas'
+    ];
+
+    public function guru()
+    {
+        return $this->hasOne(Guru::class);
+    }
+
+    public function siswa()
+    {
+        return $this->hasMany(Siswa::class);
+    }
+}
