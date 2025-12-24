a86594026e17b225ee7115d83c045bca522c6d40
Bagas Riatma Putera <145095794+Bagasbelajarhtml@users.noreply.github.com>
2025-12-24 11:51:36 +0700

    fix: migration table jenis_pelanggaran

--- database/migrations/2025_12_22_111911_create_jenis_pelanggaran_table.php
+++ database/migrations/2025_12_22_111911_create_jenis_pelanggaran_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
         Schema::create('jenis_pelanggaran', function (Blueprint $table) {
             $table->id();
             $table->string('nama_pelanggaran');
+            $table->string('kategori');
             $table->integer('poin');
             $table->timestamps();
         });
