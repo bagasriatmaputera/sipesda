edeb038a964e2a323a58bca7196be96c05f57619
Bagas Riatma Putera <145095794+Bagasbelajarhtml@users.noreply.github.com>
2025-12-26 21:25:43 +0700

    feat: add SiswaFactory

---
+++ database/factories/SiswaFactory.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Kelas;
+use App\Models\Siswa;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Siswa>
+ */
+class SiswaFactory extends Factory
+{
+    protected $model = Siswa::class;
+
+    public function definition(): array
+    {
+        return [
+            'nis' => fake()->unique()->numerify('2025#####'),
+            'nama' => fake()->name(),
+            'nama_wali' => fake()->name(),
+            'no_hp_wali' => fake()->unique()->numerify('08#########'),
+            'kelas_id' => Kelas::inRandomOrder()->first()->id ?? 1, // Mengambil ID kelas yang sudah ada
+            'total_poin' => 0,
+        ];
+    }
+}
