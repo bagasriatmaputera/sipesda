c01dbad575d1fb6571c08d40a0c18a50d38fea33
Bagas Riatma Putera <145095794+Bagasbelajarhtml@users.noreply.github.com>
2025-12-31 14:42:49 +0700

    feat: add jenisPelanggaranRequest

---
+++ app/Http/Requests/JenisPelanggaranRequest.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class JenisPelanggaranRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        if ($this->has('0')) {
+            return [
+                '*.nama_pelanggaran' => 'required|string|min:5',
+                '*.kategori' => 'required|string|min:5',
+                '*.poin' => 'required|numeric|min:1'
+            ];
+        }
+
+        if ($this->isMethod('patch')) {
+            return [
+                'nama_pelanggaran' => 'required|string|min:5',
+                'kategori' => 'nullable|string|min:5',
+                'poin' => 'nullable|numeric|min:1'
+            ];
+        }
+        return [
+            'nama_pelanggaran' => 'required|string|min:5',
+            'kategori' => 'required|string|min:5',
+            'poin' => 'required|numeric|min:1'
+        ];
+    }
+}
